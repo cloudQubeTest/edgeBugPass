a39b8a7fe020e762e964e6eb8e3b39592b6eb93e
cloudQubeTest <brintonc@seattleu.edu>
2016-11-27 20:56:06 -0800

    changed bug

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World !");
-	Calendar c = new GregorianCalendar(2014, 12, 25);  // Noncompliant
+	for (int i = 10; i < 10; i++) {  // Noncompliant 
+	}
     }
 }
