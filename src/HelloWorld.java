d2c379446405d92587a7c7a74a886b625de96717
cloudQubeTest <brintonc@seattleu.edu>
2017-03-29 13:09:25 -0700

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,5 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World !");
-	for (int i = 10; i < 10; i++) {  // Noncompliant 
-	}
     }
 }
